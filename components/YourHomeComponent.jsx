4c447d15252cf589edf151273ce5c21eb27c4d0c
Shreya Upadhya <147341463+Shreya-Upadhya@users.noreply.github.com>
2025-03-04 20:08:11 +0530

    Add files via upload

---
+++ components/YourHomeComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const YourHomeComponent = () => {
+    return (
+        <div>
+            <h1>Welcome to Your Home Component!</h1>
+        </div>
+    );
+};
+
+export default YourHomeComponent;
